b1964c85f89511f5a98199721674d55dd959930e
Yuki Tada <yuki0125.bass@gmail.com>
2025-08-28 00:53:50 +0900

    依存関係に 'jose' を追加し、セッションの暗号化と復号化を行う新しいミドルウェアを実装

---
+++ src/lib/session.ts
@@ -0,0 +1,16 @@
+import "server-only";
+import { jwtVerify } from "jose";
+
+const secretKey = process.env.JWT_ACCESS_SECRET;
+const encodedKey = new TextEncoder().encode(secretKey);
+
+export async function decrypt(session: string | undefined = "") {
+  try {
+    const { payload } = await jwtVerify(session, encodedKey, {
+      algorithms: ["HS256"],
+    });
+    return payload;
+  } catch (error) {
+    console.log("Failed to verify session");
+  }
+}
