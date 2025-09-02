e1af2bdc0b71f1f5f73f51eaf397ae9e375a902a
Yuki Tada <yuki0125.bass@gmail.com>
2025-09-02 22:32:55 +0900

    デフォルトサイドバー実装

--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -1,7 +1,14 @@
-export default function Layout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
-  return <div>{children}</div>;
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
+import { AppSidebar } from "@/components/app-sidebar"
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <SidebarProvider>
+      <AppSidebar />
+      <main>
+        <SidebarTrigger />
+        {children}
+      </main>
+    </SidebarProvider>
+  )
 }
