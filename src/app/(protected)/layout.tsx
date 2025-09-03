f7e6e52ed6fd8beb5b82801474fa42cbd90a9c52
Yuki Tada <yuki0125.bass@gmail.com>
2025-09-03 15:12:21 +0900

    レイアウトコンポーネントのスタイルを修正し、ユーザーページにボードのリストを表示する機能を追加

--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -1,14 +1,14 @@
-import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
-import { AppSidebar } from "@/components/app-sidebar"
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import { AppSidebar } from "@/components/app-sidebar";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <SidebarProvider>
       <AppSidebar />
-      <main>
+      <main className="w-full">
         <SidebarTrigger />
-        {children}
+        <div className="p-8">{children}</div>
       </main>
     </SidebarProvider>
-  )
+  );
 }
--- src/app/(protected)/u/[userId]/page.tsx
+++ src/app/(protected)/u/[userId]/page.tsx
@@ -1,7 +1,26 @@
-import React from 'react'
+import { Card } from "@/components/ui/card";
+import { cookies } from "next/headers";
+import Link from "next/link";
+import React from "react";
 
-export default function page() {
+export default async function page() {
+  const cookieHeader = cookies().toString();
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_ROUTE}/boards`, {
+    credentials: "include",
+    headers: {
+      cookie: cookieHeader,
+    },
+  });
+  const boards = await res.json();
   return (
-    <div>page</div>
-  )
+    <div className="grid grid-cols-3">
+        {boards.map((b) => (
+          <Link href={`/b/${b.id}`} key={b.id}>
+            <Card>
+              <h3 className="text-center">{b.title}</h3>
+            </Card>
+          </Link>
+        ))}
+    </div>
+  );
 }
