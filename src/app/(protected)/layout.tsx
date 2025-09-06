e6752ff88e34fdd7f248303a372b35e92ca493eb
Yuki Tada <yuki0125.bass@gmail.com>
2025-09-06 16:06:19 +0900

    Headerコンポーネントを追加し、レイアウトに統合。グローバルスタイルを更新し、フォント設定を追加。

--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -1,14 +1,18 @@
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 import { AppSidebar } from "@/components/app-sidebar";
+import Header from "@/components/header";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <SidebarProvider>
-      <AppSidebar />
-      <main className="w-full">
-        <SidebarTrigger />
-        <div className="p-8">{children}</div>
-      </main>
-    </SidebarProvider>
+    <div className="p-4 h-screen">
+      <Header />
+      <SidebarProvider className="gap-4">
+        <AppSidebar />
+        <main className="w-full rounded-2xl shadow-md p-4 bg-sidebar">
+          <SidebarTrigger className="md:hidden" />
+          {children}
+        </main>
+      </SidebarProvider>
+    </div>
   );
 }
